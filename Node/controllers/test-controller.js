2ce739c0bae9f8b0a5309a664ac398db90f8ad1f
Xaviertinoco06 <75238870+Xaviertinoco06@users.noreply.github.com>
2024-02-08 17:25:55 -0600

    Mongo Toda la Semana

---
+++ Node/controllers/test-controller.js
@@ -0,0 +1,171 @@
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_CONNECTION);
+
+async function createLibro(req, res) {
+  try {
+    const {
+      _id,
+      nombre,
+      author,
+      pag,
+      publicacion,
+      editorial,
+      ISBN,
+      genero,
+      comentarios,
+      edicion,
+    } = req.body;
+
+    const libro = {
+      _id,
+      nombre,
+      author,
+      pag,
+      publicacion,
+      editorial,
+      ISBN,
+      genero,
+      comentarios,
+      edicion,
+    };
+
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .insertOne(libro);
+    res.send(result);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
+
+async function getLibros(req, res) {
+  const result = await client
+    .db("admin")
+    .collection("libros")
+    .find({})
+    .toArray();
+  res.send(result);
+}
+
+async function updateLibros(req, res) {
+  try {
+    const {
+      id,
+      nombre,
+      author,
+      pag,
+      publicacion,
+      editorial,
+      ISBN,
+      genero,
+      comentarios,
+      edicion,
+      copias,
+      enEstante,
+    } = req.body;
+    const libro = {
+      nombre,
+      author,
+      pag,
+      publicacion,
+      editorial,
+      ISBN,
+      genero,
+      comentarios,
+      edicion,
+      copias,
+      enEstante,
+    };
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .updateOne({ _id: id }, { $set: libro });
+    res.status(200).send(result);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
+
+async function deleteLibros(req, res) {
+  try {
+    const { id } = req.body;
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .deleteOne({ _id: id });
+    res.status(200).send(result);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
+
+async function getLibroById(req, res) {
+  try {
+    const { id } = req.query;
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .findOne({ _id: parseInt(id) });
+    res.status(200).send({
+      message: "Libro encontrado",
+      data: result,
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: e.message,
+    });
+  }
+}
+
+async function createEstante(req, res) {
+  try {
+    const { id, position, categoria } = req.body;
+    const result = await client.db("admin").collection("estantes").insertOne({
+      _id: id,
+      position,
+      categoria,
+      libros: [],
+    });
+    res.status(200).send({
+      message: "Estante creado",
+      data: result,
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: e.message,
+    });
+  }
+}
+
+async function libroAEstante(req, res) {
+  try {
+    const { idEstante, idLibro } = req.body;
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .findOne({ _id: idLibro });
+    const resultEstante = await client
+      .db("admin")
+      .collection("estantes")
+      .updateOne({ _id: idEstante }, { $push: { libros: result } });
+    res.status(200).send({
+      message: "Libro agregado al estante",
+      data: resultEstante,
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: e.message,
+    });
+  }
+}
+
+module.exports = {
+  createLibro,
+  createEstante,
+  getLibros,
+  updateLibros,
+  deleteLibros,
+  getLibroById,
+  libroAEstante,
+};
--- Node/routes/users.js
+++ Node/routes/users.js
@@ -1,22 +1,40 @@
-const { axios } = require('axios');
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/weather', function (req, res, next) {
+require("dotenv").config();
+const axios = require("axios");
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_CONNECTION);
+async function run() {
   try {
-      axios.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3f94178af25220bccb09bfe6d231ca04"
-          + process.env.WHEATHER_API)
-          .then(response => {
-              res.send(response.data);
-          })
-  } catch (error) {
-      console.error(error);
-    }
-});
-module.exports = router;
+    await client.connect();
 
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// async function getWeather() {
+//   let options = {
+//     method: "GET",
+//     url:
+//       "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=" +
+//       process.env.WEATHER_API,
+//   };
+//   };
+//   const response = await axios.request(options);
+//   return response.data;
+// }
+
+router.get("/envtest", async function (req, res, next) {
+  res.send({
+    env: process.env.WEATHER_API,
+    response: await getWeather(),
+  });
+});
+
+module.exports = router;
